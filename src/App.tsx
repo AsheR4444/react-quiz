c8837837db9617987342f83b5a7bb68449d9ced2
Ilya <il08082000@gmail.com>
2022-01-03 13:51:57 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './index.css'
+
+export const App: React.FC = () => {
+    return (
+        <div>
+            <h1 className='text-3xl font-bold underline'>
+                Hello Typescript
+            </h1>
+        </div>
+    )
+}
