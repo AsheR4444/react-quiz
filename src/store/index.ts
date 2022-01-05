3bf33452e0e7c2d6c0745ff0018ec9646285897c
Ilya <il08082000@gmail.com>
2022-01-06 00:34:45 +0300

    step 1

---
+++ src/store/index.ts
@@ -0,0 +1,7 @@
+import { configureStore } from '@reduxjs/toolkit'
+
+export const configuredStore:any =  configureStore({
+    reducer: {
+
+    }
+})
---
+++ src/store/questionSlice.ts
@@ -0,0 +1,12 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const quiestionSlice = createSlice({
+    name: 'questions',
+    initialState: {
+        questions: {}
+    },
+    reducers: {
+        
+    }
+})
+ 
